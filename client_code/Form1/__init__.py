3e37790696a3e54e2c6b2ea9030d9967b4a85672
sunnykraditya@gmail.com <sunnykraditya@gmail.com>
2024-01-15 17:47:56 +0000

    Created LICENSE.txt; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -7,3 +7,15 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def JD_change(self, **event_args):
+    """This method is called when the text in this text area is edited"""
+    pass
+
+  def CV_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    pass
+
+  def button_3_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
